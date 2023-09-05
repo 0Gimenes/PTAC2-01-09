5fb007c32c5099b63e0855c6d0fd2ca02e1dfcd0
0Gimenes <gabrielgimenes9353@gmail.com>
2023-09-05 10:07:44 -0400

    Criei a primeira propriedade
    criei a propriedade chamado album

--- src/Todo/ToDo.jsx
+++ src/Todo/ToDo.jsx
@@ -6,18 +6,23 @@ export default function ToDo() {
     const [atividade, setAtividade] = useState("");
     const [lista, setLista] = useState([]);
     const [id, setId] = useState(1);
+    const [musica, setmusica] = useState(1);
+    const [album, setalbum] = useState(1);
 
     const salvar = (e) => {
         e.preventDefault();
         setLista([...lista, {
             atividade: atividade,
-            id: id
+            id: id,
+            musica: musica
         }]);
         setId(id + 1);
         setAtividade("");
     };
     const remover = (id) => {
+
         /*setLista(lista.filter((ativ) => (ativ.id !== id ? lista : null)));*/
+
         const auxLista = [];
         lista.map((lista) => {
             if (lista.id !== id) {
@@ -29,17 +34,29 @@ export default function ToDo() {
     return (
         <div class="container">
             <Link to="/">home</Link>
-            <h1>Lista de Atividades</h1>
+            <img class = "jukebox" src="https://cdn.mariapiacasa.com.br/media/catalog/product/cache/1/image/1000x/9df78eab33525d08d6e5fb8d27136e95/j/u/jukebox-classic-pequeno-_27296-01.jpg"></img>
+            <h1>Lista de Musicas</h1>
             <form onSubmit={salvar}>
+
+                  
                 <input type="text"
-                    value={atividade}
-                    onChange={(e) => { setAtividade(e.target.value) }} />
+                    value={album}
+                    onChange={(e) => { setalbum(e.target.value) }} />
+               
+          
+      
+
+            <input type="text"
+                    value={musica}
+                    onChange={(e) => { setmusica(e.target.value) }} />
                 <button>ADICIONAR</button>
             </form>
+
             {lista.map((ativ) =>
                 <ul key={ativ.id}>
                     <li>
-                        <p>{ativ.atividade}</p>
+                        <p>Album:{ativ.album}</p>
+                        <p>{ativ.musica}</p>
                         <button onClick={() => remover(ativ.id)}>Remover</button>
                     </li>
                 </ul>
